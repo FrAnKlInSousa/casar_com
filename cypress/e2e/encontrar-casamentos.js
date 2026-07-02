74622296f91eda64423d9b88905e61a8a817edf5
franklin <frank.fkn.23@gmail.com>
2026-07-02 17:31:16 -0300

    casos de teste de busca de casamentos

---
+++ cypress/e2e/encontrar-casamentos.js
@@ -0,0 +1,31 @@
+describe('Buscar de casamentos', () => {
+    it('Informando nome de casal e data', () =>{
+        cy.visit('/')
+        cy.url().should('contain', 'casar.com')
+
+        cy.contains('Encontre um casamento').click()
+        cy.url().should('contain', 'encontre-um-casamento')
+
+        cy.get('[name="coupleName"]').click().type('ana')
+        cy.get('.react-datepicker__input-container > .text-neutral')
+            .click().type('01/08/2026')
+        cy.contains('Buscar').click()
+
+        cy.get('.mb-4')
+            .should('contain', 'resultados para: ana')
+
+    })
+
+    it('Informando apenas nome de casal', ()=>{
+        cy.visit('/')
+        cy.url().should('contain', 'casar.com')
+        cy.contains('Encontre um casamento').click()
+        cy.url().should('contain', 'encontre-um-casamento')
+
+        cy.get('[name="coupleName"]').click().type('ana')
+        cy.contains('Buscar').click()
+
+        cy.get('.mb-4').should('contain', 'resultados para: ana')
+    })
+
+})
