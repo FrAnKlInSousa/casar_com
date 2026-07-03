dcdfe2dd9ad0c511302b93bd5c5d479df6a60b81
franklin <frank.fkn.23@gmail.com>
2026-07-03 14:50:06 -0300

    adicionando acesso ao site do casamento

--- cypress/e2e/encontrar-casamentos.cy.js
+++ cypress/e2e/encontrar-casamentos.cy.js
@@ -16,16 +16,20 @@ describe('Buscar de casamentos', () => {
 
     })
 
-    it('Informando apenas nome de casal', ()=>{
+    it('Informando apenas nome de casal e acessar o site', ()=>{
         cy.visit('/')
         cy.url().should('contain', 'casar.com')
         cy.contains('Encontre um casamento').click()
         cy.url().should('contain', 'encontre-um-casamento')
 
-        cy.get('[name="coupleName"]').click().type('ana')
+        cy.get('[name="coupleName"]').type('teste')
         cy.contains('Buscar').click()
 
-        cy.get('.mb-4').should('contain', 'resultados para: ana')
+        cy.get('.mb-4').should('contain', 'resultados para: teste')
+
+        cy.visitSite('tes tes teste e teste teste')
+        cy.url().should('contain', 'noivos.casar.com')
+
     })
 
 })
