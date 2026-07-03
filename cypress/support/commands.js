3a6afa2f3621f647c244e9b6eefdf38095dc4598
franklin <frank.fkn.23@gmail.com>
2026-07-03 14:49:07 -0300

    criando comando para abrir link na mesma guia

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('visitSite', (titulo) => {
+    cy.contains('h3', titulo)
+        .parent()
+        .contains('a', 'Visitar site')
+        .invoke('removeAttr', 'target')
+        .click()
+})
