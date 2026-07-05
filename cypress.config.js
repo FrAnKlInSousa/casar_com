28dff3ee3b8e3676cee927adc15ca3698ad16357
franklin <frank.fkn.23@gmail.com>
2026-07-05 20:41:17 -0300

    removendo gravacao de video e adicionando https na baseurl

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
-  video: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://casar.com',
+    baseUrl: 'https://casar.com',
     viewportWidth: 1280
   },
 });
